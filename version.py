b174125a677c7e749c1dbcd9b39816d4f28cea9f
Jon A. LLoyd <101219721+lloydcpe@users.noreply.github.com>
2024-08-01 16:09:08 -0400

    Create version.py
    Allows for versioning and info on executable file.

---
+++ version.py
@@ -0,0 +1,11 @@
+import pyinstaller_versionfile as pv
+
+pv.create_versionfile(
+	output_file = "versionfile.txt",
+	version = "1.3",
+	company_name = "Center for Policing Equity",
+	file_description = "OHRP CITI App",
+	internal_name = "CITI Update Center",
+	legal_copyright = "Center for Poling Equity. Free to reproduce with attribution.",
+	original_filename = "CITI Update Center.exe",
+	product_name = "CITI Update Center")
